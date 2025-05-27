f04d47a0ede1e6e6d0b29f2efe3a31c43396dfeb
Camille Linan <camille.linan@hotmail.com>
2025-05-27 22:59:11 +0200

    add all covers

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import Introduction from '../components/Home/Introduction/Introduction';
+import DisplayProjects from '../components/Home/DisplayProjects/DisplayProjects';
 
 const Home: FC = () => {
   return (
     <>
       <Introduction />
+      <DisplayProjects />
     </>
   );
 };
